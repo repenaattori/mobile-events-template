38e01f68feb076b782adef02d913aacaa293d3b5
repenaattori <reimarii@oamk.fi>
2022-12-27 10:50:37 +0200

    Change task descriptions

--- App.js
+++ App.js
@@ -12,14 +12,15 @@ export default function App() {
   );
 }
 
+/**
+ * Calculate sum for two integers
+ * Add all the necessary event handling for calculating the sum when pressing button
+ * You may assure the number format in calculation using e.g. Number(num1)
+ */
 function SumView(){
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [sum, setSum] = useState(0);
-
-  //Calculate sum for two integers
-  //Add all the necessary event handling for calculating the sum when pressing button
-  //You may assure the number format in calculation using e.g. Number(num1)
 
   return (
     <View style={Styles.container}>
@@ -43,16 +44,14 @@ function SumView(){
 
 /**
  * Create euros to pounds converter
+ * JS uses . in decimal numbers.
+ * You may replace comma given in UI with dot 
+ * using e.g. let withoutComma = value.replace(',','.')
+ * You may round the result to specific number of decimals by using e.g. result.toFixed(2) 
  */
 function CurrencyView(){
   const [euros, setEuros] = useState(0);
   const [pounds, setPounds] = useState(0);
-
-  //JS uses . in decimal numbers.
-  //You may replace comma given in UI with dot 
-  //using e.g. let withoutComma = value.replace(',','.')
-
-  //You may round the result to specific number of decimals by using e.g. result.toFixed(2) 
 
   return (
     <View>
